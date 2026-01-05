bbef292613b29bdd85ba97809ac39fdf192222c9
samantha-colmart <samantha.colmart@laplateforme.io>
2026-01-05 12:03:13 +0100

    ajout du job 2

---
+++ jour01/job02/index.php
@@ -0,0 +1,16 @@
+<?php
+
+// Ceci est un commentaire sur une seule ligne
+echo "Hello LaPlateforme!<br>";
+
+/*
+   Ceci est un commentaire sur plusieurs lignes (bloc)
+   Il peut s'étendre sur plusieurs lignes
+*/
+echo "Hello LaPlateforme!<br>";
+
+#Ceci est un commentaire sur une seule ligne
+echo "Hello LaPlateforme!<br>";
+
+?>
+
