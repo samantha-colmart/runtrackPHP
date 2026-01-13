14a01029eb8e76d943e3eb5727cbbcd9a214211a
samantha-colmart <samantha.colmart@laplateforme.io>
2026-01-13 10:52:22 +0100

    ajout jour7 job1

---
+++ jour07/job01/index.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['nbvisites'])) {
+    $_SESSION['nbvisites'] = 0;
+}
+
+if (isset($_POST['reset'])) {
+    $_SESSION['nbvisites'] = 0;
+}
+
+$_SESSION['nbvisites']++;
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Compteur de visites</title>
+</head>
+<body>
+
+<h1>Compteur de visites</h1>
+
+<p>Nombre de visites : <strong><?php echo $_SESSION['nbvisites']; ?></strong></p>
+
+<form method="post">
+    <button type="submit" name="reset">Reset</button>
+</form>
+
+</body>
+</html>
